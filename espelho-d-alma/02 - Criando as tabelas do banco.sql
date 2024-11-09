0449486593c6ecdfd567af1152f277cbd9a8cd0a
Gabriel Ribeiro <bielzin.senac2024@gmail.com>
2024-11-08 21:12:43 -0300

    Add files via upload

---
+++ espelho-d-alma/02 - Criando as tabelas do banco.sql
@@ -0,0 +1,49 @@
+/* Criando a tabela Unidades_Senac*/
+CREATE TABLE Unidades_Senac (
+    id_unidade INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    cep VARCHAR(10) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+
+/* Criando a tabela Alunos e Adicionando Chave Estrangeira*/
+CREATE TABLE Alunos (
+    id_aluno INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    cpf VARCHAR(13) UNIQUE NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    senha VARCHAR(30) NOT NULL,
+    fk_id_unidade INT NOT NULL,
+    FOREIGN KEY (fk_id_unidade) REFERENCES Unidades_Senac(id_unidade)
+);
+
+/* Criando a tabela Profissionais*/
+CREATE TABLE Profissionais (
+    id_profissional INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    cpf VARCHAR(13) UNIQUE NOT NULL,
+    codigo_crp INT UNIQUE NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    senha VARCHAR(40) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    foto VARCHAR(255) NOT NULL,
+    tempo_na_area VARCHAR(20) NOT NULL
+);
+
+/* Criando a tabela Agendamentos e Adicionando Chave Estrangeira*/
+CREATE TABLE Agendamentos (
+    id_agendamento INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    fk_id_aluno INT NOT NULL,
+    fk_id_profissional INT NOT NULL,
+    fk_id_unidade INT NOT NULL,
+    horario_agendamento TIME NOT NULL,
+    dia_agendamento DATE NOT NULL,
+    obs VARCHAR(255),
+    status_agendamento VARCHAR(20) DEFAULT 'AGENDANDO',
+    FOREIGN KEY (fk_id_aluno) REFERENCES Alunos(id_aluno),
+    FOREIGN KEY (fk_id_profissional) REFERENCES Profissionais(id_profissional),
+    FOREIGN KEY (fk_id_unidade) REFERENCES Unidades_Senac(id_unidade)
+);
+
